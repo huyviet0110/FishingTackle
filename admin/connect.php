19ee4e9764facf4275bd70a7592eb3c5cf432c22
Nguyễn Lương Việt Huy <huynguyenviet0110@gmail.com>
2022-09-17 09:03:43 +0700

    test

--- admin/connect.php
+++ admin/connect.php
@@ -2,3 +2,9 @@
 
 	$connect = mysqli_connect('localhost', 'root', '', 'fishing_tackle');
 	mysqli_set_charset($connect, 'utf8');
+
+	
+	$connect = mysqli_connect('localhost', 'root', '', 'fishing_tackle');
+	mysqli_set_charset($connect, 'utf8');
+	$connect = mysqli_connect('localhost', 'root', '', 'fishing_tackle');
+	mysqli_set_charset($connect, 'utf8');
