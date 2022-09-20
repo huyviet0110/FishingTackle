f1d0ad309995d22fa8d3b0efed6025a044d4813d
Nguyễn Lương Việt Huy <huynguyenviet0110@gmail.com>
2022-09-20 22:13:13 +0700

    update (add crud): types - crud

---
+++ admin/types/content.php
@@ -0,0 +1,70 @@
+<?php 
+
+	require_once '../connect.php';
+	$table_name = 'types';
+	require_once '../root/get_data.php';
+?>
+
+<div class="content">
+
+	<div class="card">
+		<div class="card-header" style="padding-top: 10px">
+			<div class="card-title">
+				<h3 class="h3-format">Types Management</h3>
+			</div>
+
+			<?php require_once '../root/card_search.php' ?>
+		</div>
+
+		<?php if($number_of_records < 1) { ?>
+			<p class="empty-records"><?php echo "There are no $table_name here!" ?></p>
+		<?php } ?>
+		
+		<div id="notification"></div>
+
+		<?php require_once '../root/card_insert.php' ?>
+		
+		<div class="card-content">
+			<table width="97%">
+				<tr>
+					<th class="th-td-id">ID</th>
+					<th class="th-td-normal">Image</th>
+					<th class="th-td-normal">Name</th>
+					<th class="th-td-description">Short description</th>
+					<th class="th-td-action">Action</th>
+				</tr>
+
+				<?php if($number_of_records > 0) { ?>
+					<?php foreach ($result as $each): ?>
+						<tr>
+							<td><?php echo trim($each['id']) ?></td>
+							<td>
+								<img src="images/<?php echo $each['image'] ?>" height="100px" width="100px">
+							</td>
+							<td><?php echo trim($each['name']) ?></td>
+							<td><?php echo mb_substr(trim($each['description']), 0, 50) . '...' ?></td>
+							<td class="th-td-action">
+								<?php include '../root/card_view.php' ?>
+							</td>
+							<td class="th-td-action">
+								<?php include '../root/card_update.php' ?>
+							</td>
+							<td class="th-td-action">
+								<?php include '../root/card_delete.php' ?>
+							</td>
+						</tr>
+					<?php endforeach ?>
+				<?php } ?>
+			</table>
+
+			<?php 
+				if($number_of_records > 0){
+					require_once '../root/show_pagination.php';
+				}
+			?>
+			
+		</div>
+	</div>
+</div>
+
+<?php require_once '../root/notification.php' ?>
---
+++ admin/types/form_insert.php
@@ -0,0 +1,91 @@
+<?php 
+	require_once '../form_validation/backend_check/page_get.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+
+	<link rel="stylesheet" type="text/css" href="../css/menu.css">
+	<link rel="stylesheet" type="text/css" href="../css/dashboard.css">
+	<link rel="stylesheet" type="text/css" href="../css/header.css">
+	<link rel="stylesheet" type="text/css" href="../css/content.css">
+	<link rel="stylesheet" type="text/css" href="../css/card.css">
+	<link rel="stylesheet" type="text/css" href="../css/notification.css">
+	<link rel="stylesheet" type="text/css" href="../css/form.css">
+	<link rel="stylesheet" type="text/css" href="../css/footer.css">
+</head>
+<body>
+	<?php require_once '../root/menu.php' ?>
+
+	<div id="dashboard">
+
+		<?php require_once '../root/header.php' ?>
+
+		<div class="content">
+
+			<div id="notification" style="margin-bottom: 20px; font-size: 20px;"></div>
+
+			<div class="form">
+				<div class="form-title">
+					<h3 class="h3-format">Add a new type</h3>
+				</div>
+				<div class="form-content">
+					<form method="post" action="process_insert.php">
+						<div class="form-input">
+							<p>Name</p>
+							<input type="text" name="name" id="input_name">
+							<div id="name_error"></div>
+						</div>
+						<input type="hidden" name="page" value="<?php echo $page ?>">
+						<div class="create-button">
+							<button type="submit" onclick="return check();">
+								Create
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+			 
+		<?php require_once '../root/footer.php' ?>
+
+	</div>
+
+	<script src="../form_validation/frontend_check/check_error.js"></script>
+	<script src="../form_validation/frontend_check/image.js"></script>
+	<script src="../form_validation/frontend_check/insert_image.js"></script>
+	<script src="../form_validation/frontend_check/name.js"></script>
+	<script src="../form_validation/frontend_check/description.js"></script>
+	<script src="../form_validation/frontend_check/phone_number.js"></script>
+	<script src="../form_validation/frontend_check/address.js"></script>
+	<script src="../form_validation/frontend_check/email.js"></script>
+	
+	<script type="text/javascript">
+		function check() {
+			let count = 0;
+			const result_check = [
+				check_name()
+			];
+			for(let i = 0; i < result_check.length; i++){
+				if(result_check[i] === true){
+					count++;
+				}
+			}
+			return (count === result_check.length) ? true : false;
+		}
+	</script>
+
+	<script src="https://kit.fontawesome.com/9741b0bef5.js" crossorigin="anonymous"></script>
+	
+	<?php 
+		require '../connect.php';
+		require_once '../root/notification.php';
+		mysqli_close($connect);
+	?>
+	
+</body>
+</html>
