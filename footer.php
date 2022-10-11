831990b52334aead9f94f9f7271083b74f6601b4
Nguyễn Lương Việt Huy <huynguyenviet0110@gmail.com>
2022-10-11 14:21:27 +0700

    update (fix homepage)
    client - homepage

---
+++ footer.php
@@ -0,0 +1,51 @@
+<div id="footer">
+	<div class="above">
+		<div class="footer-content">
+			<img src="logo/logo_2.png">
+		</div>
+		<div class="footer-content">
+			<h4>NEWSLETTER</h4><br>
+			<p style="font-style: italic;">Subscribe for exclusive offers, special events and a good tall fish tale or two.</p>
+			<br>
+			<form method="post" action="#">
+				<input type="email" name="email" placeholder="Email">
+				<button>SIGN UP</button>
+			</form>
+		</div>
+		<div class="footer-content">
+			<h4>LINKS</h4><br>
+			<p>Shop</p>
+			<p>About Us</p>
+			<p>Shipping</p>
+			<p>Terms</p>
+			<p>Privacy</p>
+			<p>Returns</p>
+		</div>
+		<div class="footer-content">
+			<h4>GET IN TOUCH</h4><br>
+			<span>Contact us </span>
+			<a href="#" style="text-decoration: underline;">info@huyviet.com</a><br><br>
+			<i class="fa-brands fa-instagram"></i>
+			<i class="fa-brands fa-facebook-f"></i>
+			<i class="fa-brands fa-pinterest-p"></i><br><br>
+			<img src="logo/logo_2.png">
+		</div>
+	</div>
+
+	<div class="below">
+		<p style="color: #a8a476;">Accepted Payments</p>
+		<i class="fa-brands fa-cc-amex"></i>
+		<i class="fa-brands fa-cc-discover"></i>
+		<i class="fa-brands fa-cc-mastercard"></i>
+		<i class="fa-brands fa-paypal"></i>
+		<i class="fa-brands fa-cc-visa"></i>
+		<br>
+		<p>
+			<i class="fa-regular fa-copyright" style="color: #a8a476; font-size: 16px;"></i>
+			<span>2022, </span>
+			<a href="index.php">FishingTackle</a>
+			<span> | Site by</span> 
+			<a href="#">Huy Viet</a>
+		</p>
+	</div>
+</div>
