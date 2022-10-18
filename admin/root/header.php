f1d484a5cb3285e5abe911a68e4bd180febd3ce8
Nguyễn Lương Việt Huy <108040632+huyviet0110@users.noreply.github.com>
2022-10-18 22:33:22 +0700

    update (fix header): admin - root - header

--- admin/root/header.php
+++ admin/root/header.php
@@ -8,7 +8,7 @@
 		<img src="../staffs/avatars/<?php echo $_SESSION['admin_avatar'] ?>" height="40px" width="40px" style="border: 1px solid white; border-radius: 50%;">
 		
 		<p><?php echo $_SESSION['admin_name'] ?></p>
-		<a href="sign_out.php">
+		<a href="../sign_out.php">
 			Sign out
 		</a>
 	</div>
