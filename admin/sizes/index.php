da5725627e32571b015761a37f001b39d6c363ad
Nguyễn Lương Việt Huy <108040632+huyviet0110@users.noreply.github.com>
2022-10-31 09:56:39 +0700

    update (add notify & add sweet alert): admin - sizes - notify & sweet alert

--- admin/sizes/index.php
+++ admin/sizes/index.php
@@ -33,10 +33,56 @@
 		require_once 'dashboard.php';
 	?>
 	
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+	<script src="../form_validation/frontend_check/notify.min.js"></script>
 	<script src="https://kit.fontawesome.com/9741b0bef5.js" crossorigin="anonymous"></script>
 	<script src="../form_validation/frontend_check/confirm_delete.js"></script>
 	
 	<?php require_once '../root/notification.php' ?>
+
+	<script>
+		$(document).ready(function() {
+			$(".btn-delete-size").click(function(event) {
+				event.preventDefault();
+				let btn = $(this);
+				let id = btn.data('id');
+
+				Swal.fire({
+					title: 'Are you sure?',
+					text: "You won't be able to revert this!",
+					icon: 'warning',
+					showCancelButton: true,
+					confirmButtonColor: '#3085d6',
+					cancelButtonColor: '#d33',
+					confirmButtonText: 'Yes, delete it!'
+				}).then((result) => {
+					if (result.isConfirmed) {
+						$.ajax({
+							url: 'delete.php',
+							type: 'POST',
+							data: {id},
+						})
+						.done(function(response) {
+							if(response == 1){
+								let parent_tr = btn.parents('tr');
+								parent_tr.remove();
+
+								$.notify("This size has been deleted.", "success");
+
+								// Swal.fire(
+								// 	'Deleted!',
+								// 	'This size has been deleted.',
+								// 	'success'
+								// )
+							}
+						});
+					}
+				})
+			});
+		});					
+	</script>
 
 	<?php mysqli_close($connect) ?>
 </body>
